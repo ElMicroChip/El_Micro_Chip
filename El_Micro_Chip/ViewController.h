bffee6ec91f9653fd430eb25c7c05744f8193f5f
Estudiante <mac_alfil_9@Mac-Alfil-9.local>
2014-08-12 20:06:26 -0500

    Initial Commit

---
+++ El_Micro_Chip/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  El_Micro_Chip
+//
+//  Created by Estudiante on 12/08/14.
+//  Copyright (c) 2014 LaMicroTienda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ El_Micro_ChipTests/El_Micro_ChipTests.m
@@ -0,0 +1,34 @@
+//
+//  El_Micro_ChipTests.m
+//  El_Micro_ChipTests
+//
+//  Created by Estudiante on 12/08/14.
+//  Copyright (c) 2014 LaMicroTienda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface El_Micro_ChipTests : XCTestCase
+
+@end
+
+@implementation El_Micro_ChipTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
